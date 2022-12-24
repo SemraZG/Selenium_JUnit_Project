97a4827b74e661b804aa9e19ddd4d5f2785cbec6
asiyecan <asiyecan.pdr@gmail.com>
2022-12-24 21:00:53 +0300

    selenium project

--- src/test/java/tests/TestCase1_RegisterUser.java
+++ src/test/java/tests/TestCase1_RegisterUser.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
-public class TestCase1_RegisterUser extends TestBase { git
+public class TestCase1_RegisterUser extends TestBase {
+
 
 
         // 1. Launch browser
