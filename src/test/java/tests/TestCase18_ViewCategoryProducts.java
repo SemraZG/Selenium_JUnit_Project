8fbd22b23e5d5a9ae6e1c769edd73a42f422dfd7
Afatsum2 <musaksac@gmail.com>
2023-01-02 22:40:22 +0300

    Q7-18

--- src/test/java/tests/TestCase18_ViewCategoryProducts.java
+++ src/test/java/tests/TestCase18_ViewCategoryProducts.java
@@ -1,15 +1,55 @@
 package tests;
 
-public class TestCase18_ViewCategoryProducts {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
-        //1. Launch browser
-        //2. Navigate to url 'https://automationexercise.com/'
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestCase18_ViewCategoryProducts extends  TestBase  {
+
+//    1. Launch browser
+//    2. Navigate to url 'http://automationexercise.com'
+//    3. Verify that categories are visible on left side bar
+//   4. Click on 'Women' category
+//   5. Click on any category link under 'Women' category, for example: Dress
+//   6. Verify that category page is displayed and confirm text 'WOMEN - TOPS PRODUCTS'
+//   7. On left side bar, click on any sub-category link of 'Men' category
+//   8. Verify that user is navigated to that category page
+
+    @Test
+
+    public void VerifyTestCasesPage() throws InterruptedException {
+        // 2. Navigate to url 'https://automationexercise.com/'
+         driver.get( "https://automationexercise.com/");
+
         //3. Verify that categories are visible on left side bar
+        Thread.sleep(3000);
+        assertTrue(driver.findElement(By.xpath("//h2")).isDisplayed());
+
         //4. Click on 'Women' category
-        //5. Click on any category link under 'Women' category, for example: Dress
+           driver.findElement(By.xpath("//a [@data-toggle='collapse']")).click();
+
+        // 5. Click on any category link under 'Women' category, for example: Dress
+           driver.findElement(By.xpath("//a[@href='/category_products/2']")).click();
+           Thread.sleep(3000);
+
         //6. Verify that category page is displayed and confirm text 'WOMEN - TOPS PRODUCTS'
+        //driver.findElement(By.xpath("//a[@href='/category_products/2']")).click();
+        String actualText1 = driver.findElement(By.xpath("//h2[@class='title text-center']")).getText();
+        assertEquals("WOMEN - TOPS PRODUCTS",actualText1);
+
+
         //7. On left side bar, click on any sub-category link of 'Men' category
+        driver.findElement(By.xpath("//a[@href='#Men']")).click();
+
+
         //8. Verify that user is navigated to that category page
+        driver.findElement(By.xpath("//a[@href='/category_products/3']")).click();
+        String actualText2 = driver.findElement(By.xpath("//h2[@class='title text-center']")).getText();
+        assertEquals("MEN - TSHIRTS PRODUCTS",actualText2);
 
 
+    }
 }
--- src/test/java/tests/TestCase7_VerifyTestCasesPage.java
+++ src/test/java/tests/TestCase7_VerifyTestCasesPage.java
@@ -1,11 +1,42 @@
 package tests;
 
-public class TestCase7_VerifyTestCasesPage {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import static org.junit.Assert.*;
+
+public class TestCase7_VerifyTestCasesPage extends TestBase {
+    private Object assertTrue;
 
         // 1. Launch browser
         // 2. Navigate to url 'https://automationexercise.com/'
-        // 3. Verify that home page is visible successfully
+        // 3. Verify that home page is visible successfullyT
         // 4. Click on 'Test Cases' button
         // 5. Verify user is navigated to test cases page successfully
+
+    @Test
+
+    public void VerifyTestCasesPage() throws InterruptedException {
+       // 2. Navigate to url 'https://automationexercise.com/'
+        driver.get( "https://automationexercise.com/");
+
+      // 3. Verify that home page is visible successfullyT
+
+        Thread.sleep(3000);
+
+        assertTrue(driver.findElement(By.xpath("//a[@href = '/products']")).isEnabled());
+
+        // 4. Click on 'Test Cases' button
+        driver.findElement(By.xpath("//a[@href = '/test_cases']")).click();
+        Thread.sleep(3000);
+        // 5. Verify user is navigated to test cases page successfully
+        assertTrue(driver.findElement(By.partialLinkText("Test Case")).isEnabled());
+
+
+    }
+
+
 
 }
