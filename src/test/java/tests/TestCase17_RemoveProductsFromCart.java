f6e7b1ed76170d303780f6d85b289708ef816f2a
Semra Gökalp <semrazengin666@gmail.com>
2023-01-04 20:19:57 +0300

    semra

--- src/test/java/tests/TestCase17_RemoveProductsFromCart.java
+++ src/test/java/tests/TestCase17_RemoveProductsFromCart.java
@@ -1,6 +1,11 @@
 package tests;
 
-public class TestCase17_RemoveProductsFromCart {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class TestCase17_RemoveProductsFromCart extends TestBase {
 
         // 1. Launch browser
         // 2. Navigate to url 'https://automationexercise.com/'
@@ -10,5 +15,26 @@ public class TestCase17_RemoveProductsFromCart {
         // 6. Verify that cart page is displayed
         // 7. Click 'X' button corresponding to particular product
         // 8. Verify that product is removed from the cart
+@Test
+    public void test() throws InterruptedException {
+    // 2. Navigate to url 'https://automationexercise.com/'
+    driver.get("https://automationexercise.com/");
+    // 3. Verify that home page is visible successfully
+    Assert.assertTrue(driver.findElement(By.id("slider")).isDisplayed());
+    // 4. Add products to cart
+    driver.findElement(By.xpath("(//a[@data-product-id='1'])[1]")).click();
+    // 5. Click 'Cart' button
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//u")).click();
+    // 6. Verify that cart page is displayed
+   Assert.assertTrue(driver.findElement(By.id("cart_items")).isDisplayed());
+    // 7. Click 'X' button corresponding to particular product
+    driver.findElement(By.xpath("//i[@class='fa fa-times']")).click();
+    // 8. Verify that product is removed from the cart
+    Thread.sleep(2000);
+   String isCartEmpty=driver.findElement(By.xpath("//b[.='Cart is empty!']")).getText();
+    System.out.println(isCartEmpty);
+   Assert.assertEquals("Cart is empty!", isCartEmpty);
 
+}
 }
--- src/test/java/tests/TestCase22_AddToCartFromRecommendedItems.java
+++ src/test/java/tests/TestCase22_AddToCartFromRecommendedItems.java
@@ -1,6 +1,14 @@
 package tests;
 
-public class TestCase22_AddToCartFromRecommendedItems {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class TestCase22_AddToCartFromRecommendedItems extends TestBase {
 
         // 1. Launch browser
         // 2. Navigate to url 'https://automationexercise.com/'
@@ -9,5 +17,30 @@ public class TestCase22_AddToCartFromRecommendedItems {
         // 5. Click on 'Add To Cart' on Recommended product
         // 6. Click on 'View Cart' button
         // 7. Verify that product is displayed in cart page
+
+    @Test
+    public void test() throws InterruptedException {
+
+        // 2. Navigate to url 'https://automationexercise.com/'
+        driver.navigate().to("https://automationexercise.com/");
+
+        // 3. Scroll to bottom of page
+        Actions actions=new Actions(driver);
+        actions.keyDown(Keys.CONTROL).sendKeys(Keys.END).perform();
+
+        // 4. Verify 'RECOMMENDED ITEMS' are visible
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='recommended_items']")).isDisplayed());
+
+        // 5. Click on 'Add To Cart' on Recommended product
+        driver.findElement(By.xpath("(//a[@data-product-id='4'])[3]")).click();
+
+        // 6. Click on 'View Cart' button
+        driver.findElement(By.xpath("//u[.='View Cart']")).click();
+
+        // 7. Verify that product is displayed in cart page
+        Thread.sleep(3000);
+        Assert.assertTrue(driver.findElement(By.xpath("//img[@src='get_product_picture/4']")).isDisplayed());
+
+    }
 
 }
